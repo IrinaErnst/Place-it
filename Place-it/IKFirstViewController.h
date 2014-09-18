9aa20665c0fa18250f248d5470bd1218f9fd2275
Irina Kalashnikova <IrinaKalashnikova@gmail.com>
2014-09-17 21:10:50 -0400

    Initial Commit

---
+++ Place-it/IKFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  IKFirstViewController.h
+//  Place-it
+//
+//  Created by Irina Kalashnikova on 9/17/14.
+//  Copyright (c) 2014 Irina Kalashnikova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKFirstViewController : UIViewController
+
+@end
