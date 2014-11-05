81bff143debbcc8999ba36d331c7d425633acff8
im-13 <i.michalowska@gmail.com>
2014-11-05 00:32:58 -0500

    Interface Place-it view added

---
+++ Place-it/Place-itViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Place-itViewController.swift
+//  Place-it
+//
+//  Created by Ilona Michalowska on 11/4/14.
+//  Copyright (c) 2014 Ilona Michalowska & Irina Kalashnikova. All rights reserved.
+//
+
+import UIKit
+
+class Place_itViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
