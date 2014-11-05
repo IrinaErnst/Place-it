75a008976f2ab1a6d96922cf94044bc318432080
Irina Kalashnikova <IrinaKalashnikova@gmail.com>
2014-11-05 12:12:21 -0500

    Added iBeacons to Xcode

--- Place-it/Place-it/ViewController.swift
+++ Place-it/Place-it/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-25556B57FE6D"), identifier: "iBeacons")
+    let colors = [
+        //key = min_value
+        30314: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        32857: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1)
+        // UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
 
     @IBOutlet weak var PlaceItButton: UIButton!
     @IBOutlet weak var InboxButton: UIButton!
@@ -22,12 +32,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typicaly from a nib.
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse)
+        {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+            let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown}
+            if (knownBeacons.count > 0){
+                let closestBeacon = knownBeacons[0] as CLBeacon
+                self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+        }
     }
 
 
