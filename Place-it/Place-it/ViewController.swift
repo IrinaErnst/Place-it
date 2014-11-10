5c25137527b5ae4ac9c652a5c8bd39d1533921a0
im-13 <i.michalowska@gmail.com>
2014-11-10 17:43:30 -0500

    Revert to 33e0f2085

---
+++ Place-it/Place-it/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Place-it
+//
+//  Created by Ilona Michalowska on 11/4/14.
+//  Copyright (c) 2014 Ilona Michalowska & Irina Kalashnikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var PlaceItButton: UIButton!
+    @IBOutlet weak var InboxButton: UIButton!
+    @IBOutlet weak var DraftsButton: UIButton!
+    @IBOutlet weak var SentButton: UIButton!
+    @IBOutlet weak var TrashButton: UIButton!
+    @IBOutlet weak var BackgroundImageView: UIImageView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
