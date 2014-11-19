4a6aaf9be65d2a90dda94c2f94ff23ceb1331b52
im-13 <i.michalowska@gmail.com>
2014-11-19 16:22:34 -0500

    table view impl for Send

---
+++ Place-it/Place-it/MessageManager.swift
@@ -0,0 +1,13 @@
+//
+//  MessageManager.swift
+//  Place-it
+//
+//  Created by Ilona Michalowska on 11/19/14.
+//  Copyright (c) 2014 Ilona Michalowska & Irina Kalashnikova. All rights reserved.
+//
+
+import UIKit
+
+class MessageManager: NSObject {
+   
+}
